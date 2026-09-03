fb7a169eda6552fae7fcbac724b762b92666df6f
kapeed234 <deepaksoundarapandi@gmail.com>
2026-09-03 22:51:55 +0530

    Remove OTP verification routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,16 +2,12 @@ const express = require("express");
 
 const {
   registerUser,
-  verifyEmail,
-  resendVerificationCode,
   loginUser
 } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
-router.post("/verify-email", verifyEmail);
-router.post("/resend-code", resendVerificationCode);
 router.post("/login", loginUser);
 
 module.exports = router;
