78a4f1470ea976fcf2bd22adfe659dc04bf01863
kapeed234 <deepaksoundarapandi@gmail.com>
2026-09-04 13:33:30 +0530

    Add registration OTP verification route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const {
   registerUser,
+  verifyRegistration,
   loginUser
 } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/verify-registration", verifyRegistration);
 router.post("/login", loginUser);
 
 module.exports = router;
